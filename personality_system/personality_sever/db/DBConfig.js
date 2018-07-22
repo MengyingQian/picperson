75e87df799391dfefcd7cee1cc245299431d2f53
MengyingQian <656206843@qq.com>
2018-07-22 19:29:29 +0800

    后台与数据库通信成功，http://localhost:3000/users/addUser?uid=4&name=helloMySQL可添加数据到数据库

---
+++ personality_system/personality_sever/db/DBConfig.js
@@ -0,0 +1,10 @@
+module.exports =
+ {  
+             mysql: {   
+                         host: '127.0.0.1',     
+                         user: 'root',   
+                         password: 'qq5099689439',  
+                         database:'test_db', // 前面建的user表位于这个数据库中 
+                          port: 3306  
+                    }
+ };
--- personality_system/personality_sever/routes/users.js
+++ personality_system/personality_sever/routes/users.js
@@ -1,32 +1,41 @@
 var express = require('express');
-var URL = require('url'); 
 var router = express.Router();
-var User = require('./user')//CommonJs如何引入封装好的模块
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// 导入MySQL模块
+var mysql = require('mysql');
+var dbConfig = require('../db/DBConfig');
+var userSQL = require('../db/Usersql');
+// 使用DBConfig.js的配置信息创建一个MySQL连接池
+var pool = mysql.createPool( dbConfig.mysql );
+// 响应一个JSON数据
+var responseJSON = function (res, ret) {
+     if(typeof ret === 'undefined') { 
+          res.json({     code:'-200',     msg: '操作失败'   
+        }); 
+    } else { 
+      res.json(ret); 
+  }};
+// 添加用户
+router.get('/addUser', function(req, res, next){
+ // 从连接池获取连接 
+pool.getConnection(function(err, connection) { 
+// 获取前台页面传过来的参数  
+ var param = req.query || req.params;   
+// 建立连接 增加一个用户信息 
+connection.query(userSQL.insert, [param.uid,param.name], function(err, result) {
+        if(result) {      
+             result = {   
+                      code: 200,   
+                     msg:'增加成功'
+             };  
+        }     
+          
+     // 以json形式，把操作结果返回给前台页面     
+       responseJSON(res, result);   
 
-router.get('/getUserInfo', function(req, res, next) {
+     // 释放连接  
+      connection.release();  
 
-  var user = new User();
-  var params = URL.parse(req.url, true).query;
-
-if(params.id == '1') {
-
-  user.name = "ligh";
-  user.age = "1";
-  user.city = "北京市";
-
-}else{  
-  user.name = "SPTING";
-  user.age = "1";
-  user.city = "杭州市";
-}
-
-var response = {status:1,data:user};
-res.send(JSON.stringify(response));
-
-});
-
+       });
+    });
+ });
 module.exports = router;
